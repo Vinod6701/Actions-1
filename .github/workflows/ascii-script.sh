4e1a85bf18cda50f28de1631365d0b32ce75e8ed
Vinod6701 <144608276+Vinod6701@users.noreply.github.com>
2025-08-26 17:13:00 +0530

    hidden

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f dragon "run for cover, iam a dragon" >> dragon.txt
+cowsay -f dragon "Run for cover, I am a dragon.....RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
